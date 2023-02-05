dc705f5eec687ed9c816eb7f2dbe020b743df77b
JAIKISHEN-MISHRA <119570134+JAIKISHEN-MISHRA@users.noreply.github.com>
2023-02-05 12:38:24 +0530

    Add files via upload

---
+++ Assignment12_String2/Q2Concate.java
@@ -0,0 +1,10 @@
+//To concat two strings we can use + operator or concate() method
+public class Q2Concate {
+    public static void main(String[] args) {
+        String name="JAI";
+        name=name.concat("Mishra");
+        System.out.println(name);
+        name=name+31;
+        System.out.println(name);
+    }
+}
